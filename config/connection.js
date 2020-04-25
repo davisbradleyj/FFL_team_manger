7323802ac21df7ee626c5479247534121754064f
guachochi <davis.bradleyj@gmail.com>
2020-04-24 17:51:11 -0700

    add heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,25 @@
 // establish MySQL connection
 var mysql = require("mysql");
-
+var connection;
 // connection details
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "taqueria_db"
-});
-// confirm success or error in making conenctions
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "taqueria_db"
+    });
+    // confirm success or error in making conenctions
+    connection.connect(function (err) {
+        if (err) {
+            console.error("error connecting: " + err.stack);
+            return;
+        }
+        console.log("connected as id " + connection.threadId);
+    });
+};
 // export for use by other files
 module.exports = connection;
